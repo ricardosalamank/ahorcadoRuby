6e5116511a59dbb1cfd9215879cd922931928630
yamit <yamit.cardenas@gmail.com>
2017-01-27 15:28:34 -0500

    added phantomjs and poltergeist to test JS

--- Gemfile
+++ Gemfile
@@ -7,5 +7,6 @@ gem 'simplecov' #coverage report
 gem 'simplecov-console' #coverage console report
 gem 'cucumber' # aceptance testting
 gem 'capybara' # framework to inspect web components
+gem 'poltergeist' #for js testing
 gem 'rack-test'# default web driver for capybara
 gem 'better_errors' #better log errors on web
--- features/support/env.rb
+++ features/support/env.rb
@@ -7,6 +7,7 @@ Sinatra::Application.app_file = File.join(File.dirname(__FILE__), *%w[.. .. app.
 
 require 'rspec/expectations'
 require 'capybara/cucumber'
+require 'capybara/poltergeist' #Javascript
 
 
 class MyWorld
@@ -15,7 +16,21 @@ class MyWorld
   include RSpec::Matchers
 end
 
-   
+Capybara.javascript_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+  options = {
+      :js_errors => true,
+      :timeout => 120,
+      :debug => false,
+      :phantomjs_options => ['--load-images=no', '--disk-cache=false'],
+      :inspector => true,
+  }
+
+  Capybara::Poltergeist::Driver.new(app, options)
+
+end
+
 Capybara.app = Sinatra::Application
 
 World{MyWorld.new}
