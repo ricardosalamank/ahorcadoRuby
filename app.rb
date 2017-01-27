422db9a38f0f8661fe1978d0c0aae7af91770f12
kleer <kleer@kleer-csd-box>
2017-01-27 14:44:43 -0500

    separated configuration from app.rb

--- app.rb
+++ app.rb
@@ -1,14 +1,6 @@
 require 'sinatra'
-require 'better_errors'
-
-configure :development do
-  use BetterErrors::Middleware
-  BetterErrors.application_root = File.expand_path('..', __FILE__)
-end
-
-enable :sessions
-set :session_secret, 'thisIsAKleerSecret'
+require './config'
 
 get '/' do
-	
+
 end
---
+++ config.rb
@@ -0,0 +1,10 @@
+
+require 'better_errors'
+
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
+
+enable :sessions
+set :session_secret, 'thisIsAKleerSecret'
